004cf376fd8b3c9ba7a9622ccdb78a3adb6a2b97
steel580 <36293338+steel580@users.noreply.github.com>
2023-11-12 00:20:34 +0900

    All done.

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
-const canvas = document.querySelector("#canvas");
+const ask = document.querySelector("#ask");
 
-for(i = 0; i <16; i++){
-    const row = document.createElement("div");
-    row.setAttribute('class', 'row');
-    canvas.appendChild(row);
-    for (j = 0; j < 16; j++){
-        const block = document.createElement("div");
-        block.setAttribute('class', 'block');
-        row.appendChild(block);
-    };
+sizeButton = document.querySelector('#resize');
+clearButton = document.querySelector('#clear');
+
+let canvasSize = 16;
+
+sizeButton.addEventListener('click', () => {
+    canvasSize = prompt("Enter the size of the canvas");
+
+    drawCanvas(canvasSize);
+});
+
+clearButton.addEventListener('click', () => {
+    let boxes = document.querySelectorAll(".block");
+    boxes.forEach((box) => {
+        box.style.background = 'white';
+    });
+})
+
+
+
+function drawCanvas(size) {
+    const canvas = document.querySelector("#canvas");
+
+    //remove all elements
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+
+    //draw the canvas using the size
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute('class', 'row');
+        canvas.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const block = document.createElement("div");
+            block.setAttribute('class', 'block');
+            block.style.background = 'hsl(240, 100%, 100%)';
+            block.dataset.lightness = 100;
+            row.appendChild(block);
+        };
+    }
+
+    //color the canvas
+    let boxes = document.querySelectorAll(".block");
+    boxes.forEach((box) => {
+
+        box.addEventListener('mouseover', function () {
+            if (isMouseDown) {
+                let lightness = parseInt(box.dataset.lightness);
+                lightness = Math.max(lightness - 10, 50);
+                box.dataset.lightness = lightness;
+                box.style.background = `hsl(240, 100%, ${lightness}%)`;
+            }
+        })
+    }
+    );
 }
+drawCanvas(16);
+
 let isMouseDown = false;
-document.addEventListener('mousedown', function(e) {
-    if (e.button ===0) {
+
+document.addEventListener('mousedown', function (e) {
+    if (e.button === 0) {
         isMouseDown = true;
     }
 });
-document.addEventListener('mouseup', function() {
+
+document.addEventListener('mouseup', function () {
     isMouseDown = false;
 });
 
-let boxes = document.querySelectorAll(".block");
 
-boxes.forEach((box) => {
-
-    box.addEventListener('mouseover', function() {
-        if (isMouseDown){
-            box.style.background = 'blue';
-        }
-    })
-
-}
-);
